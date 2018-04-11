fe5bffa325a226066c0203c8490d4d9e9004e2e0
Sam McLeod <samm@infoxchange.org>
2018-04-11 23:18:04 +1000

    minify js

--- assets/js/ie10-viewport-bug-workaround.js
+++ assets/js/ie10-viewport-bug-workaround.js
@@ -4,21 +4,4 @@
  * Copyright 2014-2017 Twitter, Inc.
  * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
  */
-
-// See the Getting Started docs for more information:
-// https://getbootstrap.com/getting-started/#support-ie10-width
-
-(function () {
-  'use strict'
-
-  if (navigator.userAgent.match(/IEMobile\/10\.0/)) {
-    var msViewportStyle = document.createElement('style')
-    msViewportStyle.appendChild(
-      document.createTextNode(
-        '@-ms-viewport{width:auto!important}'
-      )
-    )
-    document.head.appendChild(msViewportStyle)
-  }
-
-}())
+!function(){"use strict";if(navigator.userAgent.match(/IEMobile\/10\.0/)){var e=document.createElement("style");e.appendChild(document.createTextNode("@-ms-viewport{width:auto!important}")),document.head.appendChild(e)}}();
